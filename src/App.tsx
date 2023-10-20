e969ae7922adfd9b62c6714d546df68f398313ec
NikDoe <nikd0e@yandex.ru>
2023-10-20 08:56:54 +0300

    refactoring ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,16 @@
-import { useReducer } from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import { ActionType, TAction, TState } from "./types";
-
-const initialState: TState = {
-	balance: 0,
-	loan: 0,
-	isOpen: false
-};
-
-function reducer (state: TState, action: TAction): TState {
-	switch (action.type) {
-		case ActionType.OPENACCOUNT:
-			return {
-				...state,
-				balance: state.balance + action.payload,
-				isOpen: true
-			};
-		
-		case ActionType.DEPOSIT:
-			return {
-				...state,
-				balance: state.balance + action.payload
-			};
-		
-		case ActionType.WITHDRAW:
-			return {
-				...state,
-				balance: state.balance > 0 
-					? state.balance - action.payload 
-					: 0
-			};
-		case ActionType.REQUESTLOAN:
-			if(state.loan > 0) return state;
-			return {
-				...state,
-				balance: state.balance + action.payload,
-				loan: action.payload
-			};
-
-		case ActionType.PAYLOAN:
-			return {
-				...state,
-				balance: state.balance - state.loan,
-				loan: 0
-			};
-
-		case ActionType.CLOSEACCOUNT:
-			if(state.balance !== 0 || state.loan > 0) return state;
-			return initialState;
-
-		default:
-			return state;
-	}
-}
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
 
 function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
-
 	return (
-		<div className="app">
-			<Header />
-			<Main 
-				state={state} 
-				dispatch={dispatch}
-			/>
+		<div>
+			<h1>🏦 The React-Redux Bank ⚛️</h1>
+			<CreateCustomer />
+			<Customer />
+			<AccountOperations />
+			<BalanceDisplay />
 		</div>
 	);
 }
